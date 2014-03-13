a78f7824548d09d82c9e0a04c3f71f7bbb65e42f
Buck Golemon <workitharder@gmail.com>
2014-03-13 14:26:28 -0700

    failing test for byte/unicode concat

--- Lib/test/test_future_encoding.py
+++ Lib/test/test_future_encoding.py
@@ -10,5 +10,22 @@ class TestExplicitEncoding(unittest.TestCase):
     def test_doesnt_crash(self):
         self.assertTrue(True)
 
+    def test_concat(self):
+
+        self.assertRaisesRegexp(
+            TypeError,
+            "^Can't convert 'bytes' object to str implicitly$",
+            b'foo'.__add__,
+            u'bar',
+        )
+        self.assertRaisesRegexp(
+            TypeError,
+            "^Can't convert 'bytes' object to str implicitly$",
+            u'foo'.__add__,
+            b'bar',
+        )
+
+
+
 def test_main():
     test_support.run_unittest(TestExplicitEncoding)
